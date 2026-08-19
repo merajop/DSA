79f0edf9ec39e79436c1c2796467aa0c113bb7bd
Muhammad Meraj <merajjio131@gmail.com>
2026-08-20 02:11:05 +0530

    Create CountEven_Odd_Numbers.java

---
+++ Arrays/Basics/CountEven_Odd_Numbers.java
@@ -0,0 +1,30 @@
+
+
+public class CountEven_Odd_Numbers {
+
+    public static void main(String[] args) {
+        int[] nums = { 11, 1, 20, 3, 23, 10 };
+
+        int even = 0;
+        int odd = 0;
+
+        // Traverse every element
+        for (int num : nums) {
+
+            // Even number → remainder is 0
+            if (num % 2 == 0) {
+                even++;
+            }
+
+            // Otherwise number is odd
+            else {
+                odd++;
+            }
+
+        }
+        System.out.println("Count Of even Numbers : " + even);
+        System.out.println("Count Of Odd Numbers : " + odd);
+
+    }
+
+}
