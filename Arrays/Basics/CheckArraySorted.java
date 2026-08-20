e1ec6df611a288ae2132e20a59fd5fc08a47f750
Muhammad Meraj <merajjio131@gmail.com>
2026-08-20 18:23:01 +0530

    Update CheckArraySorted.java

--- Arrays/Basics/CheckArraySorted.java
+++ Arrays/Basics/CheckArraySorted.java
@@ -1,16 +1,22 @@
-public class CheckArraySorted{
 
     public static void main(String[] args) {
         int[]arr={1,2,3,4,5};
 
         boolean check=true;
+        // Check whether array is sorted or not
+        int[] arr = {1, 2, 3, 4, 5};
 
         for(int i=0;i<arr.length-1;i++){
 
             if(arr[i]>arr[i+1]){
                 check=false;
                 break;
+        for (int i = 1; i < arr.length; i++) {
 
+            // Current element should be >= previous element
+            if (arr[i] < arr[i - 1]) {
+                isSorted = false;
+                break;
             }
         }
 
@@ -19,6 +25,10 @@ public class CheckArraySorted{
         }
         else{
             System.out.println("not sorted");
+        if (isSorted) {
+            System.out.println("Sorted");
+        } else {
+            System.out.println("Not Sorted");
         }
     }
 }
